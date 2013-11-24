5b0abb7557f33b036111fdd15969e90efe8d0d28
fabricior <fabriciorateni@gmail.com>
2013-11-24 11:31:11 -0300

    Test 03: One Spare

--- BowlingGame/Scripts/default.js
+++ BowlingGame/Scripts/default.js
@@ -1,11 +1,27 @@
-﻿function Game() {    
-    this.scoreCounter = 0;
+﻿function Game() {        
+    this.rolls = new Array();
+    this.currentRoll = 0;
 }
 
-Game.prototype.roll = function (pins) {
-    this.scoreCounter += pins;
+Game.prototype.roll = function (pins) {    
+    this.rolls[this.currentRoll++] = pins;
 };
 
-Game.prototype.score = function() {
-    return this.scoreCounter;
+Game.prototype.score = function () {
+    var scoreTotal = 0;
+    var frameIndex = 0;
+    for (var frame = 0; frame < 10; frame++) {
+        if (this.isSpare(frameIndex)) { 
+            scoreTotal += 10 + this.rolls[frameIndex + 2];
+            frameIndex += 2;
+        } else {
+            scoreTotal += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+            frameIndex += 2;
+        }        
+    }    
+    return scoreTotal;
+};
+
+Game.prototype.isSpare = function (frameIndex) {
+    return this.rolls[frameIndex] + this.rolls[frameIndex + 1] == 10;
 };
--- BowlingGame/Scripts/tests.js
+++ BowlingGame/Scripts/tests.js
@@ -1,22 +1,41 @@
 ﻿/********** Test Setup ***********/
-var g = new Game();
+
+var g = null;
+
+module('main module', {
+    setup: function () {
+        g = new Game();
+    }
+});
 
 /********** Helper functions *****/
 
-function rollMany(n , pins) {
+function rollMany(n, pins) {
     for (var i = 0; i < n; i++) {
         g.roll(pins);
     }
+}
+
+function rollSpare() {
+    g.roll(5);
+    g.roll(5);
 }
 
 /********** The tests ************/
 
 test('Test Gutter Game', function () {
     rollMany(20, 0);
-    equal(g.score(), 0);            
+    equal(g.score(), 0);
 });
 
-test('Test All ones', function () {    
+test('Test All ones', function () {
     rollMany(20, 1);
     equal(g.score(), 20);
+});
+
+test('Test one spare', function () {
+    rollSpare();
+    g.roll(3);
+    rollMany(17, 0);
+    equal(g.score(), 16);
 });
